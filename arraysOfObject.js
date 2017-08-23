e3c333ae6bae202aa35d2fbb72e8e763e4758402
giri68 <giri.pdt@gmail.com>
2017-08-23 11:56:18 -0700

    Added new file arraysOfObject and created array of object and accesses its value

---
+++ arraysOfObject.js
@@ -0,0 +1,30 @@
+'use strict';
+const jon = {
+  name: "Jon",
+  job_title: "Teacher"
+};
+
+const giri = {
+  name: "Giri",
+  job_title: "Developer"
+};
+
+const eric = {
+  name: "Eric",
+  job_title: "Full stack developer"
+};
+
+const rich = {
+  name: "Rich",
+  job_title: "Instructer"
+};
+
+const pavan = {
+  name: "Pavan",
+  job_title: "Student"
+};
+const arr = [jon, giri, eric, rich, pavan];
+//console.log(arr.forEach(obj => `${obj[job_title]} and ${obj[name]}` ));
+//console.log(arr[0].job_title);
+console.log(arr.map(obj => `${obj.job_title} and ${obj.name}` 
+));
